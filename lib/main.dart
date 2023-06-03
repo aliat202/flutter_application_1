860cc4ada294d966b0ce3592d4d1fad6e8f71f7e
aliat202 <62912119+aliat202@users.noreply.github.com>
2023-06-03 13:54:20 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Facebook'),
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.blue,
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              print("object");
+            },
+          ),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.search),
+              tooltip: 'Open shopping cart',
+              onPressed: () {
+                // handle the press
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.message),
+              onPressed: () {},
+            )
+          ],
+        ),
+        body: Container(
+          color: Color.fromARGB(255, 206, 201, 201),
+        ),
+      ),
+    );
+  }
+}
